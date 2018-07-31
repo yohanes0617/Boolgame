b4d67b2d8d4df907151fd697d90aa3ca499c7031
DESKTOP-3N4H189\ヨハネス <yohanes0617@gmail.com>
2018-07-31 23:23:09 +0900

    タイトルを追加

---
+++ komike/Assets/Title.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Title : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.S))
+        {
+            SceneManager.LoadScene("StageSerect");
+        }
+        if (Input.GetKey(KeyCode.T))
+        {
+            SceneManager.LoadScene("bool");
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            Application.Quit();
+        }
+    }
+}
